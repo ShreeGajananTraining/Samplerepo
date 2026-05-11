66ac001634976bb0f8dd3c1b9b2002141b3c905c
PayalS <pvsshreegajanan@gmail.com>
2026-05-11 12:32:03 +0530

    fifth playwright commit

---
+++ src/test/java/gettingstarted/UploadFile.java
@@ -0,0 +1,28 @@
+package gettingstarted;
+
+import java.nio.file.Path;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class UploadFile {
+
+	public static void main(String[] args) {
+	Browser browser =Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+Page page = browser.newPage();
+page.navigate("https://the-internet.herokuapp.com/upload");
+page.locator("#file-upload").setInputFiles(Path.of(System.getProperty("user.dir")+"/Files/Screenshot.png"));
+
+/*Path files[] = {Path.of(System.getProperty("user.dir")+"/files/Screenshot.png"),
+		Path.of(System.getProperty("user.dir")+"/files/Screenshot-Copy1.png")
+page.locator("#file-upload").setInputFiles(files);
+
+};
+*/
+
+
+	}
+
+}
